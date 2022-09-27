17f7efc38fcd93d86bcf44bc451125027330e5c4
nakido <nakidosama@outlook.com>
2022-09-27 20:47:38 +0800

    init

---
+++ cmake/Modules/MSVCPermissiveBehavior.cmake
@@ -0,0 +1,2 @@
+add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/utf-8>)
+add_compile_options($<$<CXX_COMPILER_ID:MSVC>:/permissive->)
---
+++ src.single/template.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, const char *argv[])
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    return 0;
+}
